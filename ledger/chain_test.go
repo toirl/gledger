faa3669e330c8b98dd1c68ec42170f62dc3bcb68
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-08-03 14:51:58 +0200

    Added testcase to build a new Chain with 1000 Blocks.

--- ledger/chain_test.go
+++ ledger/chain_test.go
@@ -1,11 +1,23 @@
 package ledger
 
 import "testing"
+import "strconv"
 
 func TestNewChain(t *testing.T) {
 	chain := NewChain()
 	genesisBlock := chain.last()
 	if genesisBlock.Payload != GENESIS_PAYLOAD {
 		t.Error("Expected '%s' as Payload in GenesisBlock, got '%s'", GENESIS_PAYLOAD, genesisBlock.Payload)
+	}
+}
+
+func TestBuildChain(t *testing.T) {
+	chain := NewChain()
+	for i := 1; i <= 1000; i++ {
+		block := NewBlock(strconv.Itoa(i))
+		chain.add(block)
+	}
+	if len(chain.blocks) != 1001 {
+		t.Error("Expected len of chain to be 1001, got '%s'", len(chain.blocks))
 	}
 }
