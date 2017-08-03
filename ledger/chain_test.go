59158289f0cb8a3eba3cae9d9208218a00b44bd0
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-08-03 22:47:27 +0200

    Removed Address check as Address is now different every time.

--- ledger/chain_test.go
+++ ledger/chain_test.go
@@ -13,7 +13,6 @@ func TestNewChain(t *testing.T) {
 }
 
 func TestBuildChain(t *testing.T) {
-	var want_address string = "Foo"
 	chain := NewChain()
 	for i := 1; i <= 1000; i++ {
 		block := NewBlock(strconv.Itoa(i))
@@ -21,9 +20,6 @@ func TestBuildChain(t *testing.T) {
 	}
 	if len(chain.blocks) != 1001 {
 		t.Error(fmt.Errorf("Expected len of chain to be 1001, got %d", len(chain.blocks)))
-	}
-	if chain.last().Address != want_address {
-		t.Error(fmt.Errorf("Expected Version %s, got %s", want_address, chain.last().Address))
 	}
 	if chain.last().Index != 1001 {
 		t.Error(fmt.Errorf("Expected Index %d, got %d", 1001, chain.last().Index))
